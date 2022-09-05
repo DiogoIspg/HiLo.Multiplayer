b7b05ec779f15b279e0a221122442a4a0ea892c2
DgoNevesIsep <94259068+DgoNevesIsep@users.noreply.github.com>
2022-09-06 00:44:43 +0100

    Add project files.

---
+++ HiLo.Multiplayer.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HiLo.Multiplayer.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
